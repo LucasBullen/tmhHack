84594ccb7ace8ecf8e6c8eec1d33e9c741051b8f
Grant Zeidler <grantz123@gmail.com>
2016-11-19 02:11:09 -0500

    Again, just reebasing

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,5 +1,15 @@
 console.log(chrome.extension.getBackgroundPage().testString);
+var emotionInfo;
+chrome.runtime.onMessage.addListener(function(message,sender) {
+	if (message.source === "background") {
+		console.log("Received successfully");
+		emotionInfo = message.lastValues;
+	}
+});
+
 window.onload = function() {
-  var info = localStorage.lastValues;
-  document.getElementById("anger").innerHTML = "TESTING AHAHA";
+  console.log(localStorage.lastValues);
+  if (emotionInfo != null) {
+    document.getElementById("anger").innerHTML = emotionInfo.anger;
+  }
 };
