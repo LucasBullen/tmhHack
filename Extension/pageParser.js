edc53a31f03eb86f0edc53ef2c95327b70396ba9
LucasBullen <bullenlucas@gmail.com>
2016-11-18 20:22:35 -0500

    no longer in load

--- Extension/pageParser.js
+++ Extension/pageParser.js
@@ -1,7 +1,11 @@
 window.onload = function(){
-   	var txt = document.body.innerHTML;
+   	var txt = htmlToText(document.body.innerHTML);
+}
+
+
+function htmlToText(html){
    	var removeScripts = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
     var htmlTagRegex = /(<([^>]+)>)/ig;
     var extraSpaceRegex = /\s\s+/g;
-    console.log(txt.replace(removeScripts , "\n").replace(htmlTagRegex , "\n").replace(extraSpaceRegex, ' '));
+    return html.replace(removeScripts , "\n").replace(htmlTagRegex , "\n").replace(extraSpaceRegex, ' ');
 }
